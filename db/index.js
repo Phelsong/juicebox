1ff7f6f03b73a841606691d094c420c1ccc10655
Sammy_Ester <sester2@outlook.com>
2022-03-24 13:24:50 -0500

    Login stored locally

--- db/index.js
+++ db/index.js
@@ -1,14 +1,12 @@
-require('dotenv').config();
-const LOGIN = process.env.DB_login 
-
+require("dotenv").config();
+const LOGIN = process.env.DB_login;
+console.log(LOGIN);
 // inside db/index.js
-const { Client } = require('pg'); // imports the pg module
+const { Client } = require("pg"); // imports the pg module
 
 // supply the db name and location of the database
 const client = new Client(`postgres://${LOGIN}@localhost:5432/juicebox-dev`);
 
-
-
 module.exports = {
   client,
-}
+};
