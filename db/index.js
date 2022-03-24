a3c1fca27ad5e0e83f0fc5bac5cf7b39acbbc4c0
Josh S Wilkinson <JoshsWilkinson@Outlook.com>
2022-03-24 14:26:56 -0500

    end of day1

--- db/index.js
+++ db/index.js
@@ -1,12 +1,49 @@
 require("dotenv").config();
 const LOGIN = process.env.DB_login;
-console.log(LOGIN);
+
 // inside db/index.js
 const { Client } = require("pg"); // imports the pg module
 
 // supply the db name and location of the database
 const client = new Client(`postgres://${LOGIN}@localhost:5432/juicebox-dev`);
 
+
+async function getAllUsers() {
+  const { rows } = await client.query(
+    `SELECT id, username 
+    FROM users;
+  `);
+
+  return rows;
+}
+
+async function createUser({ username, password }) {
+  try {
+    const result = await client.query(`
+      INSERT INTO users(username, password) 
+      VALUES($1, $2) 
+      ON CONFLICT (username) DO NOTHING 
+      RETURNING *;
+    `, [username, password]);
+
+    return result;
+  } catch (error) {
+    throw error;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = {
-  client,
+  client, getAllUsers, createUser
 };
+
