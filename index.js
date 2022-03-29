4a8ed108809f303d240c2ba20e68e963df8d3a5c
Josh S Wilkinson <JoshsWilkinson@Outlook.com>
2022-03-29 11:20:15 -0500

    starting Routes

---
+++ index.js
@@ -0,0 +1,34 @@
+const PORT = 3000;
+const express = require('express');
+const server = express();
+const apiRouter = require('./api');
+server.use('/api', apiRouter);
+const morgana = require('morgan');
+server.use(morgana('dev'));
+
+server.use(express.json())
+
+server.listen(PORT, () => {
+  console.log('The server is up on port', PORT)
+});
+
+
+//four parameter needs error, request, response and next, in that order
+//--
+server.use((req, res, next) => {
+    console.log("<____Body Logger START____>");
+    console.log(req.body);
+    console.log("<_____Body Logger END_____>");
+  
+    next();
+  });
+//--
+app.use('/api', (req, res, next) => {
+    console.log("A request was made to /api");
+    next();
+  });
+//---
+app.get('/api', (req, res, next) => {
+    console.log("A get request was made to /api");
+    res.send({ message: "success" });
+});
