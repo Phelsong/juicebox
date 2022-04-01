20143eddbd31a93fa63780c8510d2a7f9005cd8c
Josh S Wilkinson <JoshsWilkinson@Outlook.com>
2022-04-01 10:22:10 -0500

    day 3 finished

--- db/index.js
+++ db/index.js
@@ -300,6 +300,13 @@ async function addTagsToPost(postId, tagList) {
         FROM posts
         WHERE id=$1;
       `, [postId]);
+
+      if (!post) {
+        throw {
+          name: "PostNotFoundError",
+          message: "Could not find a post with that postId"
+        };
+      }
   
       const { rows: tags } = await client.query(`
         SELECT tags.*
--- index.js
+++ index.js
@@ -29,7 +29,7 @@ server.listen(PORT, () => {
 
 
 //four parameter needs error, request, response and next, in that order
-//--
+//--------------------------------------------------------
 server.use((req, res, next) => {
     console.log("<____Body Logger START____>");
     console.log(req.body);
@@ -37,6 +37,18 @@ server.use((req, res, next) => {
   
     next();
   });
+//--------------------------------------------------------
+server.get('/background/:color', (req, res, next) => {
+  res.send(`
+    <body style="background: ${ req.params.color };">
+      <h1>Hello World</h1>
+    </body>
+  `);
+});
+
+
+
+
 //--
 // app.use('/api', (req, res, next) => {
 //     console.log("A request was made to /api");
