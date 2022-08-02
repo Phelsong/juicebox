d7de6ffe72ee9609f899e0ab119aa3655fc13e6f
Sammy_Ester <sester2@outlook.com>
2022-08-02 10:21:36 -0500

    These are all the commits from the computer

--- api/posts.js
+++ api/posts.js
@@ -1,54 +1,49 @@
-const express = require('express');
+const express = require("express");
 const postsRouter = express.Router();
-const { getAllPosts } = require('../db');
-const {requireUser} = require('./utils');
+const { getAllPosts } = require("../db");
+const { requireUser } = require("./utils");
 
 postsRouter.use((req, res, next) => {
   console.log("A request is being made to /users");
-  
-  next();     
+
+  next();
 });
 
-postsRouter.get('/', async (req, res) => {
+postsRouter.get("/", async (req, res) => {
   const posts = await getAllPosts();
-  
+
   res.send({
-    posts
+    posts,
   });
 });
 
-postsRouter.post('/', requireUser, async (req, res, next) => {
-  res.send({ message: 'under construction' });
-
+postsRouter.post("/", requireUser, async (req, res, next) => {
+  res.send({ message: "under construction" });
 });
 
-postsRouter.post('/posts/route', requireUser, async (req, res, next) => {
+postsRouter.post("/posts/route", requireUser, async (req, res, next) => {
   const { title, content, tags = "" } = req.body;
 
-  const tagArr = tags.trim().split(/\s+/)
-  const postData = {authorId, title, content};
+  const tagArr = tags.trim().split(/\s+/);
+  const postData = { authorId, title, content };
 
   // only send the tags if there are some to send
   if (tagArr.length) {
     postData.tags = tagArr;
-    
-   
   }
 
   try {
-    const post = await createPost(postData); 
-    
-    res.send({ post })
+    const post = await createPost(postData);
+
+    res.send({ post });
     // add authorId, title, content to postData object
     // const post = await createPost(postData);
     // this will create the post and the tags for us
     // if the post comes back, res.send({ post });
-    // otherwise, next an appropriate error object 
+    // otherwise, next an appropriate error object
   } catch ({ name, message }) {
     next({ name, message });
-
   }
-});
 });
 
 module.exports = postsRouter;
