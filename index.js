419a7330a9c49ab2071aa6efb95c8459a916348b
Sammy_Ester <sester2@outlook.com>
2022-08-22 13:42:01 -0500

    Fixing Heroku issue

--- index.js
+++ index.js
@@ -1,56 +1,41 @@
-const { PORT = 3000 } = process.env
-const express = require('express');
+const PORT = process.env.PORT || 3000;
+const express = require("express");
 const server = express();
-const apiRouter = require('./api');
-const morgan = require('morgan');
-const { client } = require('./db');
-require('dotenv').config();
+const apiRouter = require("./api");
+const morgan = require("morgan");
+const { client } = require("./db");
+require("dotenv").config();
 
 client.connect();
-server.use(morgan('dev'));
-server.use(express.json())
-server.use('/api', apiRouter);
-
-
-
+server.use(morgan("dev"));
+server.use(express.json());
+server.use("/api", apiRouter);
 
 server.listen(PORT, () => {
-  console.log('The server is up on port', PORT)
+  console.log("The server is up on port", PORT);
 });
 
 //--------------
 
 //------------------
 
-
-
-
-
-
-
-
-
-
 //four parameter needs error, request, response and next, in that order
 //--------------------------------------------------------
 server.use((req, res, next) => {
-    console.log("<____Body Logger START____>");
-    console.log(req.body);
-    console.log("<_____Body Logger END_____>");
-  
-    next();
-  });
+  console.log("<____Body Logger START____>");
+  console.log(req.body);
+  console.log("<_____Body Logger END_____>");
+
+  next();
+});
 //--------------------------------------------------------
-server.get('/background/:color', (req, res, next) => {
+server.get("/background/:color", (req, res, next) => {
   res.send(`
-    <body style="background: ${ req.params.color };">
+    <body style="background: ${req.params.color};">
       <h1>Hello World</h1>
     </body>
   `);
 });
-
-
-
 
 //--
 // app.use('/api', (req, res, next) => {
@@ -62,4 +47,3 @@ server.get('/background/:color', (req, res, next) => {
 //     console.log("A get request was made to /api");
 //     res.send({ message: "success" });
 // });
-
