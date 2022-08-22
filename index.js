d081c2f89449a4d259570065b6be4c75ad22168f
Sammy_Ester <sester2@outlook.com>
2022-08-22 13:53:16 -0500

    These are the commented out code

--- index.js
+++ index.js
@@ -15,35 +15,35 @@ server.listen(PORT, () => {
   console.log("The server is up on port", PORT);
 });
 
-//--------------
+// //--------------
 
-//------------------
+// //------------------
 
-//four parameter needs error, request, response and next, in that order
-//--------------------------------------------------------
-server.use((req, res, next) => {
-  console.log("<____Body Logger START____>");
-  console.log(req.body);
-  console.log("<_____Body Logger END_____>");
+// //four parameter needs error, request, response and next, in that order
+// //--------------------------------------------------------
+// server.use((req, res, next) => {
+//   console.log("<____Body Logger START____>");
+//   console.log(req.body);
+//   console.log("<_____Body Logger END_____>");
 
-  next();
-});
-//--------------------------------------------------------
-server.get("/background/:color", (req, res, next) => {
-  res.send(`
-    <body style="background: ${req.params.color};">
-      <h1>Hello World</h1>
-    </body>
-  `);
-});
-
-//--
-// app.use('/api', (req, res, next) => {
-//     console.log("A request was made to /api");
-//     next();
-//   });
-// //---
-// app.get('/api', (req, res, next) => {
-//     console.log("A get request was made to /api");
-//     res.send({ message: "success" });
+//   next();
 // });
+// //--------------------------------------------------------
+// server.get("/background/:color", (req, res, next) => {
+//   res.send(`
+//     <body style="background: ${req.params.color};">
+//       <h1>Hello World</h1>
+//     </body>
+//   `);
+// });
+
+// //--
+// // app.use('/api', (req, res, next) => {
+// //     console.log("A request was made to /api");
+// //     next();
+// //   });
+// // //---
+// // app.get('/api', (req, res, next) => {
+// //     console.log("A get request was made to /api");
+// //     res.send({ message: "success" });
+// // });
